e4f0eec4f99403a3bc16a16f3c71e678c931659f
dark-king-001 <piyushat115@gmail.com>
2023-04-20 22:12:31 +0530

    Added header page

---
+++ Frontend/src/components/header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+export default function header() {
+  return (
+    <header>
+        <a href="/" id="logo" className="logo">
+            <img
+            src="/public/logo.png"
+            alt="VichaarShala"
+            height="100px"
+            width="100px"
+            />
+        </a>
+        <section id="navbar" className="navbar">
+             <Link to="/"># HOME</Link>
+            <Link to="/explore"># EXPLORE</Link>
+            <Link to="/messages"># MESSAGES</Link>
+            <Link to="/notifications"># NOTIFICATIONS</Link>
+            <Link to="/bookmark"># BOOKMARK</Link>
+            <Link to="/profile"># PROFILE</Link>
+            <Link to="/more"># MORE</Link>
+        </section>
+        <div className="newpost">
+            <button>New Thought</button>
+        </div>
+        <div className="user">
+        </div>
+    </header>
+  )
+}
