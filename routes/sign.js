bd690e0fda870cc01782b6f6cec39d9ce1439b62
dark-king-001 <piyushat115@gmail.com>
2023-05-21 02:13:38 +0530

    removed view route

--- routes/sign.js
+++
@@ -1,9 +0,0 @@
-var express  = require('express');
-let router = express.Router();
-
-const sign_controller = require("../controllers/sign")
-
-router.get('/signin',sign_controller.sign_in)
-router.get('/signup',sign_controller.sign_up)
-
-module.exports = router
