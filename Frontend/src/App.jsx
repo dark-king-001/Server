ce8b7efd56851f6f9a7414e27c3fe47e6d2abc46
dark-king-001 <piyushat115@gmail.com>
2023-05-21 02:11:35 +0530

    Added AppContext

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,26 +1,32 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/header.jsx"
-import Home from "./components/home.jsx"
-import Explore from "./components/explore.jsx"
-import Messages from "./components/messages.jsx"
-import Notifications from "./components/notifications.jsx"
-import Bookmarks from "./components/bookmark.jsx"
-import Profile from "./components/profile.jsx"
+import React, { useState, useEffect, useContext } from 'react';
+import { AppContext } from './AppContext';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/header.jsx';
+import Home from './components/home.jsx';
+import Explore from './components/explore.jsx';
+import Messages from './components/messages.jsx';
+import Notifications from './components/notifications.jsx';
+import Bookmarks from './components/bookmark.jsx';
+import Profile from './components/profile.jsx';
+import Login from './components/login.jsx';
+import Register from './components/register.jsx';
+
+import './App.css';
+
+export default function App() {
+  const { globalData, setGlobalData } = useContext(AppContext);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
 
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    // Check if globalData?.loggedin is true and update the isLoggedIn state accordingly
+    setIsLoggedIn(globalData?.loggedin || false);
+  }, [globalData]);
 
   return (
-    /* <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button> */
-    
     <BrowserRouter>
-      <Header />
+      {isLoggedIn && <Header />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/explore" element={<Explore />} />
@@ -28,9 +34,55 @@ function App() {
         <Route path="/notifications" element={<Notifications />} />
         <Route path="/bookmarks" element={<Bookmarks />} />
         <Route path="/profile" element={<Profile />} />
+        {!isLoggedIn && <Route path="/login" element={<Login />} />}
+        {!isLoggedIn && <Route path="/register" element={<Register />} />}
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App
+// store global data
+// import React, { useContext, useState } from 'react';
+// import axios from 'axios';
+// import { AppContext } from './AppContext';
+
+// function Login() {
+//   const { setResponseData } = useContext(AppContext);
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     axios.post('/login', { email, password })
+//       .then((response) => {
+//         setResponseData(response.data); // Store the response data in global state
+//         console.log(response.data);
+//       })
+//       .catch((error) => {
+//         console.error(error);
+//       });
+//   };
+
+//   // Rest of the component code
+// }
+
+// retrieve globaldata
+// import React, { useContext } from 'react';
+// import { AppContext } from './AppContext';
+
+// function UserProfile() {
+//   const { responseData } = useContext(AppContext);
+
+//   if (responseData) {
+//     const { email, username, accessToken } = responseData;
+
+//     // Use the extracted data in your component
+//     console.log('Email:', email);
+//     console.log('Username:', username);
+//     console.log('Access Token:', accessToken);
+//   }
+
+//   return (
+//     // Component JSX
+//   );
+// }
---
+++ Frontend/src/AppContext.jsx
@@ -0,0 +1,13 @@
+import React, { createContext, useState } from 'react';
+
+export const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [globalData, setGlobalData] = useState(null);
+
+  return (
+    <AppContext.Provider value={{ globalData, setGlobalData }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
