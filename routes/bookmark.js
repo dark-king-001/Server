6b1befb04d4c980b3a9079984e91882112d276e4
dark-king-001 <piyushat115@gmail.com>
2023-05-20 16:14:13 +0530

    removed useless route

--- routes/bookmark.js
+++
@@ -1,9 +0,0 @@
-var express  = require('express');
-let router = express.Router();
-
-const bookmark_controller = require("../controllers/bookmark")
-
-router.get('/bookmark', bookmark_controller.load_Bookmark)
-router.post('/bookmark',bookmark_controller.save_Bookmark)
-
-module.exports = router
