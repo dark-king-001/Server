16d35180e28a5347996c00e068447f3c1c8bef8a
dark-king-001 <piyushat115@gmail.com>
2023-05-21 13:24:02 +0530

    optimized header

--- Frontend/src/components/header.jsx
+++ Frontend/src/components/header.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from 'react';
+import { AppContext } from '../AppContext';
 import {Link} from 'react-router-dom'
 
 export default function header() {
+  const { globalData, setGlobalData } = useContext(AppContext);
+  const [user , setUser] = useState('');
+  useEffect(() => {
+    // Check if globalData?.loggedin is true and update the isLoggedIn state accordingly
+    setUser(globalData?.username);
+  }, [globalData]);
   return (
     <header>
         <a href="/" id="logo" className="logo">
@@ -13,18 +20,15 @@ export default function header() {
             />
         </a>
         <section id="navbar" className="navbar">
-            <Link to="/"> <p className="icon">#</p><p>HOME</p></Link>
-            <Link to="/explore"><p className="icon">#</p><p>EXPLORE</p></Link>
-            <Link to="/messages"><p className="icon">#</p><p>MESSAGES</p></Link>
-            <Link to="/notifications"><p className="icon">#</p><p>NOTIFICATIONS</p></Link>
-            <Link to="/bookmarks"><p className="icon">#</p><p>BOOKMARKS</p></Link>
-            <Link to="/profile"><p className="icon">#</p><p>PROFILE</p></Link>
-            <Link to="/more"><p className="icon">#</p><p>MORE</p></Link>
+            <Link to="/"> <p>HOME</p></Link>
+            <Link to="/explore"><p>EXPLORE</p></Link>
+            <Link to="/messages"><p>MESSAGES</p></Link>
+            <Link to="/notifications"><p>NOTIFICATIONS</p></Link>
+            <Link to="/bookmarks"><p>BOOKMARKS</p></Link>
+            <Link to="/profile"><p>PROFILE</p></Link>
         </section>
-        <div className="newpost">
-            <button><p>New Thought</p></button>
-        </div>
         <div className="user">
+          {user}
         </div>
     </header>
   )
